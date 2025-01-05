5dce4a67df9ff7166a855292b5a15ea27c8d1c9b
miamii11036 <lmiah7492@gmail.com>
2025-01-06 01:39:52 +0800

    D3OK

--- 2024/Day3/solution.py
+++ 2024/Day3/solution.py
@@ -30,3 +30,28 @@ origin_data = read_txt("clue.txt")
 # filter_data = extract_patterns(origin_data)
 # Answer = answer(filter_data)
 # print(Answer)
+
+
+def filter_data(txt):
+    mul_pattern = r"mul\((\d+),(\d+)\)"
+    do_pattern = r"do\(\)"
+    dont_pattern = r"don't\(\)"
+
+    do_status = True
+    total_sum = 0
+
+    extract_data = re.finditer(f"{mul_pattern}|{do_pattern}|{dont_pattern}", txt)
+    for value in extract_data:
+        if value.group(1) and value.group(2):
+            if do_status:
+                x = int(value.group(1))
+                y = int(value.group(2))
+                total_sum += x * y
+        elif value.group(0) == "do()":
+            do_status = True
+        elif value.group(0) == "don't()":
+            do_status = False
+    return total_sum
+
+Answer2 = filter_data(origin_data)
+print(Answer2)
